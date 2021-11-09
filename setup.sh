534c06b70d13e81ea10bcb9a8b00f4f323a64d3b
David Guzmán <49773538+davidguzmanr@users.noreply.github.com>
2021-11-09 12:37:32 -0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# See https://discourse.ubuntu.com/t/lubuntu-18-04-error-running-sudo-apt-update-sudo-apt-upgrade/4725
+sudo apt clean
+sudo apt autoclean
+sudo apt update && sudo apt upgrade -y
+
+# Install Google Chrome, see https://linuxways.net/ubuntu/how-to-install-google-chrome-on-ubuntu-20-04-lts/
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+google-chrome # The first launch must be from the terminal, I think
+
+# Install Sublime Text, see https://linuxize.com/post/how-to-install-sublime-text-3-on-ubuntu-20-04/
+sudo apt install curl
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt install sublime-text -y
+
+# Install Visual Studio Code, see https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-20-04/
+sudo snap install --classic code
+
+# Install Git
+sudo apt install git -y
+
+
+
+# References
+#  - See https://askubuntu.com/questions/519/how-do-i-write-a-shell-script-to-install-a-list-of-applications
+#  - See https://askubuntu.com/questions/409025/permission-denied-when-running-sh-scripts
