49c1b4b1b413724d069532b469384fb2057c2a9b
David Guzmán <49773538+davidguzmanr@users.noreply.github.com>
2021-11-09 19:13:54 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,18 +12,14 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 # Install Visual Studio Code, see https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-20-04/
 sudo snap install --classic code
 
-# Install Sublime Text, see https://linuxize.com/post/how-to-install-sublime-text-3-on-ubuntu-20-04/
-sudo apt install curl -y
-sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-sudo apt install sublime-text -y
+# Install Zoom, see https://linuxhint.com/install_zoom_ubuntu-2/
+sudo snap install zoom-client
 
 # Install Git
 sudo apt install git -y
 
-# Install Zoom, see https://linuxhint.com/install_zoom_ubuntu-2/
-sudo snap install zoom-client
+# Others
+sudo apt install curl -y
 
 # References
 #  - See https://askubuntu.com/questions/519/how-do-i-write-a-shell-script-to-install-a-list-of-applications
